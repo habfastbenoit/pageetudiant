88aa01e204f591fab3fde9695bd24e7cc96973d7
Benoit Habfast <habfast.benoit@gmail.com>
2017-05-18 22:57:36 +0100

    page caval

---
+++ javascript.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('#button').on('click', function(event){
+    window.scrollBy(0,180);
+  });
+ // the DOM is now ready, you can traverse it,
+ // and register events with on().
+});
